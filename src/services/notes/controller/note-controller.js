61dc2c65ba3b60c5495402eac81dfafa52ba821f
Khafidha Sukma <khafidhasukma@gmail.com>
2026-04-05 22:00:01 +0700

    feat: restructure notes app with error handling and validation

---
+++ src/services/notes/controller/note-controller.js
@@ -0,0 +1,76 @@
+import { nanoid } from 'nanoid';
+import notes from '../notes.js';
+import { InvariantError, NotFoundError } from '../../../exceptions/index.js';
+import response from '../../../utils/response.js';
+
+export const createNote = (req, res, next) => {
+  const { title = 'untitled', tags, body } = req.body;
+  const id = nanoid(16);
+  const createdAt = new Date().toISOString();
+  const updatedAt = createdAt;
+
+  const newNote = { title, tags, body, id, createdAt, updatedAt };
+  notes.push(newNote);
+
+  const isSuccess = notes.filter((note) => note.id === id).length > 0;
+
+  if (!isSuccess) {
+    return next(new InvariantError('Catatan gagal ditambahkan'));
+  }
+
+  return response(res, 201, 'Catatan berhasil ditambahkan', { noteId: id });
+};
+
+export const getAllNotes = (req, res) => {
+  const { title = '' } = req.query;
+
+  if (title !== '') {
+    const note = notes.filter((note) => note.title === title);
+    return response(res, 200, 'success', { notes: note });
+  }
+
+  return response(res, 200, 'success', { notes });
+};
+
+export const getNoteById = (req, res, next) => {
+  const { id } = req.params;
+  const note = notes.find((n) => n.id === id);
+
+  if (!note) {
+    return next(new NotFoundError('Catatan tidak ditemukan'));
+  }
+
+  return response(res, 200, 'Catatan sukses ditampilkan', { note: note });
+};
+
+export const editNoteById = (req, res) => {
+  const { id } = req.params;
+  const { title, tags, body } = req.body;
+  const updatedAt = new Date().toISOString();
+
+  const idx = notes.findIndex((n) => n.id === id);
+  if (idx !== -1) {
+    notes[idx] = { ...notes[idx], title, tags, body, updatedAt };
+    return res.json({
+      status: 'success',
+      message: 'Catatan berhasil diperbarui',
+    });
+  }
+
+  return res.status(404).json({
+    status: 'fail',
+    message: 'Gagal memperbarui catatan. Id tidak ditemukan',
+  });
+};
+
+export const deleteNoteById = (req, res, next) => {
+  const { id } = req.params;
+  const index = notes.findIndex((n) => n.id === id);
+
+  if (index === -1) {
+    return next(new NotFoundError('Catatan tidak ditemukan'));
+  }
+
+  notes.splice(index, 1);
+  return response(res, 200, 'Catatan berhasil dihapus');
+};
